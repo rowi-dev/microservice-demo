b5a3775b55e278d435add0abc0aa153c0090104b
roshan.wijendra <roshan.wijendra@inovaitsys.com>
2021-02-06 21:46:29 +0530

    init commit from local

---
+++ member-service-demo/src/main/java/com/rowi/rentacar/memberservicedemo/MemberServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rowi.rentacar.memberservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberServiceDemoApplication.class, args);
+	}
+
+}
---
+++ member-service-demo/src/test/java/com/rowi/rentacar/memberservicedemo/MemberServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowi.rentacar.memberservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
